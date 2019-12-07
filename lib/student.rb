ac5094d37658e37b9865cd8411a11d517834b32c
Alex Velasquez <jtinnesco@gmail.com>
2019-12-07 00:14:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,3 @@
-# Remember, you can access your database connection anywhere in this class
-#  with DB[:conn]
-
 class Student
   attr_accessor :name, :grade
   attr_reader :id
@@ -42,5 +39,4 @@ class Student
     student.save
     student
   end
-
 end
